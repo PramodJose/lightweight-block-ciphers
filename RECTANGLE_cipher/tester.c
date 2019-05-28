293ffb64cc0ef7c76766e8f4355a7f5f35403080
PramodJose <pramod.gj17@gmail.com>
2019-05-28 21:53:41 +0530

    Simplified the tester code. We can input different keys and plaintexts as shown in this tester code.

--- RECTANGLE_cipher/tester.c
+++ RECTANGLE_cipher/tester.c
@@ -3,83 +3,74 @@
 #include "rectangle.h"
 #define HEX(x) (x > 9 ?('A' + (x - 10)) :('0' + x))
 
+
+void print_info()
+{
+	printf("Note:\tWhile analyzing this output, please keep in mind that every pair of bytes is\n");
+	printf("\tflipped because of the little-endian architecture of Intel CPUs.\n");
+	printf("\tFor example, if the string in hexadecimal is displayed as: \"1186 F53E B458 5F8B\",\n\tthen, internally it is represented as:\n");
+	printf("\t\t8611\n\t\t3EF5\n\t\t58B4\n\t\t8B5F\n\t[The 64-bit cipher state is represented as a matrix such as this one.\n\tRefer to Figure 1 in the paper for more information on this.]\n");
+	printf("\tIn other words, the LSB is displayed first (little-endianess) in a pair of bytes\n\tdisplayed below. Each pair of bytes is separated by a space for ease of readability.\n\n");
+	printf("==================================================================================================\n\n");
+}
+
+void print_in_hex(string_t string)
+{
+	for(size_t i = 0; i < string->length; ++i)
+	{
+		char low = string->str[i] & 0x0f;
+		char high = ((unsigned char)string->str[i]) >> 4;
+		printf("%c%c", HEX(high), HEX(low));
+
+		if(i & 1)
+			printf(" ");
+	}
+
+	printf("\nAnd as a string:\t");
+	for(size_t i = 0; i < string->length && string->str[i] != 0; ++i)
+		printf("%c", string->str[i]);
+	
+	printf("\nString length:\t\t%ld\n\n", string->length);
+}
+
 void main()
 {
 	int i, j;
 
-	// Testing key generation...
-	uint32_t main_key[] = {	0xFFFFFFFF,
-							0xFFFFFFFF,
-							0xFFFFFFFF,
-							0xFFFFFFFF
+	// Input your 128 bit key here (as hex)...
+	uint32_t main_key[] = {	0x00000000,
+							0x00000000,
+							0x00000000,
+							0x00000000
 						};
 
 	rcipher_params_t cipher_params = rectangle_init_key(main_key);
 
-	for(i = 0; i < 25; ++i)
-	{
-		if(i != 24)
-			get_round_key(cipher_params);
-		else
-			get_key25(cipher_params);
-					
-		printf("Sub-key of round %d:\n", i + 1);
+	// Input your plaintext here..
+	// ..either as a byte array..
+	char ptext[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	string_t plaintext = init_string_bytes(ptext, sizeof(ptext));
 
-		for(j = 0; j < 4; ++j)
-			printf("%04x\n", cipher_params->sub_key[j]);
-		printf("\n");
-	}
-
-	rectangle_destroy(cipher_params);
+	// ..or as a string.
+	/*char *ptext = "The author of this piece of code is Pramod.";
+	string_t plaintext = init_string(ptext);*/
 	
-
-	// Testing encryption...
-	printf("\n\nTesting encryption...\n");
-	cipher_params = rectangle_init_key(main_key);
-	char ptext[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-	ciphertext_t ctext_struct;
-	printf("PText in string:\t%s\n", ptext);
+	print_info();
 	printf("Plaintext in hex:\t");
-	int len = strlen(ptext) + 1;
+	print_in_hex(plaintext);
 
-	for(i = 0; i < len; ++i)
-	{
-		char low = ptext[i] & 0x0f;
-		char high = ((unsigned char)ptext[i]) >> 4;
-		printf("%c%c", HEX(high), HEX(low));
-
-		if(i & 1)
-			printf(" ");
-	}
-	printf("\n\n");
-
-	ctext_struct = encrypt(cipher_params, ptext);
+	string_t ciphertext, decrypted_ptext;
+	ciphertext = encrypt(cipher_params, plaintext);
 
 	printf("Ciphertext in hex:\t");
-	for(i = 0; i < ctext_struct->length; ++i)
-	{
-		char low = ctext_struct->ctext[i] & 0x0f;
-		char high = ((unsigned char)ctext_struct->ctext[i]) >> 4;
-		printf("%c%c", HEX(high), HEX(low));
+	print_in_hex(ciphertext);
 
-		if(i & 1)
-			printf(" ");
-	}
-	printf("\nCText as string: \t");
-	for(i = 0; i < ctext_struct->length; ++i)
-		printf("%c", ctext_struct->ctext[i]);
-		
-	destroy_ctext(ctext_struct);
+	decrypted_ptext = decrypt(cipher_params, ciphertext);
+	printf("Decrypted ptext in hex:\t");
+	print_in_hex(decrypted_ptext);
 
+	destroy_string(decrypted_ptext);
+	destroy_string(plaintext);
+	destroy_string(ciphertext);
 	rectangle_destroy(cipher_params);
-	printf("\n");
 }
-
-
-/*
-	Correct output:
-	1110 1000 0011 1110		e83e
-	1110 1111 1110 1110		efee
-	0100 1010 0001 0101		4a15
-	0111 1010 0100 0110		7a46
-*/
